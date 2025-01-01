f8490cffe47aed516a7573c20c1e6fe6f54d7659
SebC750 <SebC89999@gmail.com>
2025-01-01 00:46:03 -0500

    Added new logo for billiards sol y luna - Created a new logo to give the establishment a unique graphic brand to replace the previous placeholder image.

--- billiards_website/src/pages/components/Navbar.tsx
+++ billiards_website/src/pages/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = (props: {isHomepage: boolean}) => {
             <header>
                 <nav className={`${scrolled || !props.isHomepage ? 'bg-black shadow-xl' : 'bg-transparent shadow-none'} flex flex-col md:flex-row justify-start items-center fixed top-0 p-2 w-full transition-all duration-250 z-10`}>
                     <div className="flex items-center p-2">
-                        <h1 className="cursor-pointer text-xl sm:text-xl md:text-2xl pl-4"> <a href="/" className="font-fascinate no-underline ml-2 transition-all text-yellow-400 mr-4 hover:text-3xl"> Billiards Sol y Luna </a></h1>
+                        <h1 className="cursor-pointer text-xl sm:text-xl md:text-2xl pl-4"> <a href="/" className="flex items-center gap-2 font-fascinate no-underline ml-2 transition-all text-yellow-400 mr-4 hover:text-3xl"> <img src="images/sunandmoon.webp" width="50" height="50" alt="billiards sol y luna logo"/> Billiards Sol y Luna </a></h1>
                     </div>
                     <div className="flex space-x-0 md:space-x-4 flex-col w-full md:flex-row md:w-auto">
                         <Link to="/" className="cursor-pointer transition-all text-yellow-400 no-underline font-fascinate p-1 sm:p-2 text-center rounded-none md:rounded-md hover:bg-yellow-400 hover:text-black display-block md:w-100 text-md md:text-base" aria-label="Home page navbar link"> Pagina Principal </Link>
